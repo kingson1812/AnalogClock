b7155909af74c723f85b8ad6c4c5821ca2bb0c8b
TranSon <kingson.a12@gmail.com>
2016-12-27 22:53:28 +0700

    !First commit. Add new project AnalogClock.

---
+++ AnalogClock/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Timers;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+
+namespace AnalogClock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Timer clockTimer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Source.MouseEventHandler.CreateInstance();
+
+            RotateLongNeedle.Angle = DateTime.Now.Second * 6 - 90;
+            RotateMediumNeedle.Angle = DateTime.Now.Minute * 6 -90;
+            RotateShortNeedle.Angle = DateTime.Now.Hour * 30 + DateTime.Now.Minute * 0.5 - 90;//1h = 30d + 1m = 0.5d
+
+            clockTimer = new Timer();
+            clockTimer.Interval = 10;
+            clockTimer.Elapsed += ClockTimer_Elapsed;
+            clockTimer.Start();
+        }
+
+        private void ClockTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+           
+            this.Dispatcher.Invoke(DispatcherPriority.Normal, (Action)(() =>
+            {
+                //Console.WriteLine(CanvasMediumClockNeedle.Width + " " + CanvasMediumClockNeedle.Height);
+                RotateLongNeedle.Angle = DateTime.Now.Second * 6 - 90;
+                RotateMediumNeedle.Angle = DateTime.Now.Minute * 6 - 90;
+                RotateShortNeedle.Angle = DateTime.Now.Hour * 30 + DateTime.Now.Minute * 0.5 - 90;
+
+                //RotateLongNeedle.Angle += 1;// DateTime.Now.Second * 6 - 90;
+                //RotateMediumNeedle.Angle += 1;// DateTime.Now.Minute * 6 - 90;
+                //RotateShortNeedle.Angle += 1;// DateTime.Now.Hour * 30 + DateTime.Now.Minute * 0.5 - 90;
+            }));
+        }
+
+        private void ImageBlankClock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Source.MouseEventHandler.GetInstance().OnLeftClickDown();
+            this.DragMove();
+        }
+    }
+}
---
+++ AnalogClock/Source/MouseEventHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AnalogClock.Source
+{
+   public class MouseEventHandler
+    {
+        public MouseEventHandler()
+        { }
+
+        public static MouseEventHandler GetInstance()
+        {
+            return s_Instance;
+        }
+
+        public static void CreateInstance()
+        {
+            s_Instance = new MouseEventHandler();
+        }
+
+        public void OnLeftClickDown()
+        {
+            
+        }
+
+        private static MouseEventHandler s_Instance;
+
+    }
+}
