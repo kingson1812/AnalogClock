2be1d60b1f65e3a516ebc8e6ec90811124c40eba
TranSon <kingson.a12@gmail.com>
2017-07-01 13:40:20 +0700

    + Correct needle angles and modify asset

--- AnalogClock/MainWindow.xaml.cs
+++ AnalogClock/MainWindow.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Timers;
-using System.Windows.Media.Animation;
 using System.Windows.Threading;
 
 namespace AnalogClock
@@ -20,7 +18,7 @@ namespace AnalogClock
             Source.MouseEventHandler.CreateInstance();
 
             RotateLongNeedle.Angle = DateTime.Now.Second * 6 - 90;
-            RotateMediumNeedle.Angle = DateTime.Now.Minute * 6 -90;
+            RotateMediumNeedle.Angle = DateTime.Now.Minute * 6 - 90;
             RotateShortNeedle.Angle = DateTime.Now.Hour * 30 + DateTime.Now.Minute * 0.5 - 90;//1h = 30d + 1m = 0.5d
 
             clockTimer = new Timer();
@@ -29,22 +27,28 @@ namespace AnalogClock
             clockTimer.Start();
         }
 
+        /// <summary>
+        /// Calculate clock angles
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ClockTimer_Elapsed(object sender, ElapsedEventArgs e)
         {
-           
+
             this.Dispatcher.Invoke(DispatcherPriority.Normal, (Action)(() =>
             {
-                //Console.WriteLine(CanvasMediumClockNeedle.Width + " " + CanvasMediumClockNeedle.Height);
-                RotateLongNeedle.Angle = DateTime.Now.Second * 6 - 90;
-                RotateMediumNeedle.Angle = DateTime.Now.Minute * 6 - 90;
+                Console.WriteLine("DateTime.Now " + DateTime.Now.Second);
+                RotateLongNeedle.Angle = (DateTime.Now.Second-1) * 6 + ((double)DateTime.Now.Millisecond/1000.0d)*6 - 90;
+                RotateMediumNeedle.Angle = DateTime.Now.Minute * 6 + DateTime.Now.Second * 0.1 - 90;
                 RotateShortNeedle.Angle = DateTime.Now.Hour * 30 + DateTime.Now.Minute * 0.5 - 90;
-
-                //RotateLongNeedle.Angle += 1;// DateTime.Now.Second * 6 - 90;
-                //RotateMediumNeedle.Angle += 1;// DateTime.Now.Minute * 6 - 90;
-                //RotateShortNeedle.Angle += 1;// DateTime.Now.Hour * 30 + DateTime.Now.Minute * 0.5 - 90;
             }));
         }
 
+        /// <summary>
+        /// Handle mouse input
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ImageBlankClock_MouseDown(object sender, MouseButtonEventArgs e)
         {
             Source.MouseEventHandler.GetInstance().OnLeftClickDown();
